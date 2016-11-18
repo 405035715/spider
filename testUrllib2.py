3804a29a1b85e9c0cf63e35c82a7f2955d6c3ae0
yuehuazhang <405035715@qq.com>
2016-11-18 20:24:11 +0800

    testUrllib2.py

--- testUrllib2.py
+++ testUrllib2.py
@@ -14,11 +14,24 @@ def urllib_test03():
     data = urllib.parse.urlencode(values)           # 编码工作
     data = data.encode('utf-8')
 
-    req = urllib.request.Request(url, data)         # 发送请求同时传data表单
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'   #模拟浏览器
+    headers = {'User-Agent': user_agent}
+
+    req = urllib.request.Request(url, data, headers)         # 发送请求同时传data表单
     the_page = ''
-    with urllib.request.urlopen(req) as response:  # 接受反馈的信息
-        the_page = response.read().decode('utf-8')  # 读取反馈的内容
-        return the_page
+    try:
+        with urllib.request.urlopen(req) as response:  # 接受反馈的信息
+            the_page = response.read().decode('utf-8')  # 读取反馈的内容
+            # for k,v in response.getheaders():   # 打印HTTP响应头
+                # print('%s: %s' % (k, v))
+            # print(response.geturl())  #打印重定向的链接
+            # print(response.info())  # 打印HTTP响应头
+            # print()
+            return the_page
+    except urllib.request.URLError as e:
+        if hasattr(e, 'code'):
+            print(e.code)
+        print(e)
 
 
 if __name__ == '__main__':
